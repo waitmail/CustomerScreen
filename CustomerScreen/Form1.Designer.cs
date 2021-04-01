de22f17267f7cbb77bfb4a84c39a635bf62038bd
gaa <waitmail@yandex.ru>
2021-04-01 11:31:49 +0300

    Добавьте файлы проекта.

---
+++ CustomerScreen/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CustomerScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbl_to_pay = new System.Windows.Forms.Label();
+            this.lbl_sum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(663, 706);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(673, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(675, 653);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // lbl_to_pay
+            // 
+            this.lbl_to_pay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbl_to_pay.AutoSize = true;
+            this.lbl_to_pay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_to_pay.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_to_pay.Location = new System.Drawing.Point(676, 674);
+            this.lbl_to_pay.Name = "lbl_to_pay";
+            this.lbl_to_pay.Size = new System.Drawing.Size(203, 41);
+            this.lbl_to_pay.TabIndex = 6;
+            this.lbl_to_pay.Text = "К ОПЛАТЕ";
+            // 
+            // lbl_sum
+            // 
+            this.lbl_sum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_sum.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_sum.Location = new System.Drawing.Point(1149, 674);
+            this.lbl_sum.Name = "lbl_sum";
+            this.lbl_sum.Size = new System.Drawing.Size(199, 39);
+            this.lbl_sum.TabIndex = 7;
+            this.lbl_sum.Text = "0.00";
+            this.lbl_sum.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 730);
+            this.ControlBox = false;
+            this.Controls.Add(this.lbl_sum);
+            this.Controls.Add(this.lbl_to_pay);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Экран покупателя";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbl_to_pay;
+        private System.Windows.Forms.Label lbl_sum;
+    }
+}
+
---
+++ CustomerScreen/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace CustomerScreen
+{
+    public partial class Form1 : Form
+    {
+        //public delegate void load_datain_datagrid(DataTable dataTable);
+        public delegate void set_data_in_cash_programm(DataTable dataTable);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.Shown += Form1_Shown;
+        }
+
+
+        public class CustomerScreen
+        {
+            public List<CheckPosition> ListCheckPositions { get; set; }
+        }
+
+        public class CheckPosition
+        {
+            public string NamePosition { get; set; }
+            public string Quantity { get; set; }
+            public string Price { get; set; }
+        }
+       
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+            receiveThread.Start();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Load("IMG_9198.jpg");            
+        }
+        
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+                this.Dispose();
+            }         
+        }
+
+        public void set_data_in_cash_programm_in_datagrid(DataTable dataTable)
+        {            
+            dataGridView1.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 14);
+            dataGridView1.DataSource = dataTable;
+            dataGridView1.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            
+
+            dataGridView1.Columns[0].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.Columns[0].Width = 450;
+            dataGridView1.Columns[1].Width = 70;
+            dataGridView1.Columns[2].Width = 100;         
+            dataGridView1.Columns[2].DefaultCellStyle.Format = "##.00";
+
+            dataGridView1.ClearSelection();
+            decimal to_pay = 0;
+            foreach (DataRow row in dataTable.Rows)
+            {
+                to_pay += (Convert.ToDecimal(row[1].ToString()) * Convert.ToDecimal(row[2].ToString()));
+            }
+            lbl_sum.Text = to_pay.ToString("### ### ###.##");
+        }
+
+        public void load_datain_datagrid(DataTable dataTable)
+        {                      
+            Invoke(new set_data_in_cash_programm(set_data_in_cash_programm_in_datagrid), new object[] { dataTable });         
+        }
+
+        private void ReceiveMessage()
+        {
+            UdpClient receiver = new UdpClient(12345); // UdpClient для получения данных
+            IPEndPoint remoteIp = null; // адрес входящего подключения
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp); // получаем данные
+                    string message = Encoding.UTF8.GetString(data);
+                    //Console.WriteLine("Собеседник: {0}", message);                    
+                    CustomerScreen customerScreen = JsonConvert.DeserializeObject<CustomerScreen>(message);
+                    DataTable dataTable = new DataTable();
+                    dataTable.Columns.Add("Наименование");
+                    dataTable.Columns.Add("Количество", new Int32().GetType());
+                    dataTable.Columns.Add("Цена", new Decimal().GetType());                    
+                    foreach (CheckPosition checkPosition in customerScreen.ListCheckPositions)
+                    {
+                        DataRow row = dataTable.NewRow();
+                        row[0] = checkPosition.NamePosition;
+                        row[1] = checkPosition.Quantity;
+                        row[2] = checkPosition.Price;                        
+                        dataTable.Rows.Add(row);                        
+                    }                                        
+                    load_datain_datagrid(dataTable);                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+    }
+}
